913a4741f1cf5b435c3978fb64d581e9f1684cf1
Umar-37 <omaralhussan@outlook.com>
2021-08-15 19:21:13 +0300

    version 1.2

---
+++ script.js
@@ -0,0 +1,12 @@
+let num="22"
+console.log(+num)
+console.log(+"22")
+
+console.log((100).toString())
+console.log(100..toString())
+console.log(`omar is \
+<h1>annd </h1>
+`);
+let ara=[1,2,4,5,6,7,87,7545343434]
+console.log(ara.sort())
+console.log(ara.join(`|`) )
