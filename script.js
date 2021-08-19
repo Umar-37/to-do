d56a478fe3b7f79d90762857d3ce927908cb5d04
Umar-37 <omaralhussan@outlook.com>
2021-08-19 21:04:29 +0300

    version 1.4

--- script.js
+++ script.js
@@ -2,36 +2,43 @@ const listsContainer=document.querySelector('[data-lists]')
 const newListForm=document.querySelector(`[data-new-list-form]`)
 const newListInput=document.querySelector(`[data-new-list-input]`)
 
+const LOCAL_STORAGE_LIST_KEY='task.lists'
+const LOCAL_STORAGE_SELECTED_LIST_ID_KEY='task.selectedListId'
 
-let lists=[{
-id:1,
-name: "solve homework"
-},{
-id:2,
-name:"sleep"}]
+let lists=JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY))
+|| [];//get local storge if have,if not then create empty arry
+let selectedListId=localStorage.getItem
+(LOCAL_STORAGE_SELECTED_LIST_ID_KEY);// return null if not exisit or empty
 
 newListForm.addEventListener(`submit`,e=>{
     e.preventDefault()
     const listName=newListInput.value;
-    console.log(listName)
+
     if(listName==null || listName==="")return
+
     const list=createList(listName)
-    newListInput.Value=null
+    newListInput.value=null
     lists.push(list)
-    console.log(lists[2].id)
-    render()
+    saveAndRrender()
 })
 
 function createList(name){
-    // this.element={
-    //     id:lists.length,
-    //     name:element.Value}
-    // lists.push(this.element)
-    // console.log(this.element)
-    return {id:Date.now().toString,
+    // this.name={
+    //     id:lists.length+1,
+    //     name:name}
+    return {id:Date.now().toString(),
             name:name,
             tasks:[]}
 
+}
+
+function saveAndRrender(){
+    save()
+    render()
+}
+
+function save(){
+    localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists))
 }
 
 function render(){
@@ -41,6 +48,9 @@ function render(){
         listElement.dataset.listId=list.id
         listElement.classList.add('list-name');
         listElement.innerText=list.name;
+
+        if(list.id===selectedListId) listElement.classList.add('active-list')
+
         listsContainer.appendChild(listElement);
     })
 }
